05ea95a5e3ff29ffebb3ae9f8a863bbc1177bdd5
devjangid2005 <jangiddev564@gmail.com>
2024-06-18 19:54:08 +0530

    react project

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let [items, setItems] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("http://localhost:8000/");
+      const jsonData = await data.json();
+      // console.log(jsonData)
+      setItems(jsonData)
+    };
+    fetchData();
+  }, []);
+  return (
+    <>
+      {items.map((item, index) => (
+        <div className="bg-white">
+          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+              Customers also purchased
+            </h2>
+            <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+              <div className="group relative">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                  <img
+                    src={item.image}
+                    alt="Front of men's Basic Tee in black."
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                      <a href="#">
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        Basic Tee
+                      </a>
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500">Black</p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">$35</p>
+                </div>
+              </div>
+              {/* More products... */}
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default App;
